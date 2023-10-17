80419acd8b2a89dfa1503999776f9f4b8e5907ec
henry1479 <fokys.81@mail.ru>
2023-10-17 15:52:38 +0300

    correcting errors

--- app/Services/GeocodingService.php
+++ app/Services/GeocodingService.php
@@ -19,7 +19,7 @@ class GeocodingService
     private function getData(string | null $address): array | null {
 
         try {
-            $ch = curl_init('https://geocode-maps.yandex.ru/1.x/?apikey=a8128129-f196-4639-981d-51e8e5424205&format=json&geocode=' . urlencode($address));
+            $ch = curl_init('https://geocode-maps.yandex.ru/1.x/?apikey= '. $_ENV["API_YANDEX_KEY"] .'.&format=json&geocode=' . urlencode($address));
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
             curl_setopt($ch, CURLOPT_HEADER, false);
